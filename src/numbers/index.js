351bea58e7476a067ff3099e5b002f1692ff0142
NicolePate <nicole.pate512@gmail.com>
2020-07-29 12:00:01 -0500

    all test passing

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,23 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+if(num % 2 === 0) {
+  return true;
 }
+};
+
+// num % 2 === 0 ? true : false;
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+let sum = arr.reduce(function(a,b){
+  return a+b;
+})
+return sum;
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  return false;
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,15 +1,24 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let subStr = "";
+  let arrOfPairs = [];
+  for (let i = 0; i < str.length; i+=2){
+    subStr = str[i] + str[i+1];
+    arrOfPairs.push(subStr);
+  }
+  return arrOfPairs;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+  const arr = str.split('');
+  arr.reverse();
+  return arr.join('');
 
 }
 
